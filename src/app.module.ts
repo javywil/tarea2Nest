0218a6f08d1836e58e0ac3466248b9370589b9cc
javywil <condorimachicado@gmail.com>
2025-12-27 13:38:32 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,23 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: "postgres",
       host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT, 10),
+      port: parseInt(process.env.POSTGRES_PORT!),
       username: process.env.POSTGRES_USERNAME,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
-      ssl: false,
+      ssl: process.env.POSTGRES_SSL === "true",
+      extra:{
+        ssl:
+        process.env.POSTGRES_SSL === "true"
+        ?{
+          rejectUnauthorized:false
+        }
+        :null,
+      }
     }),
     SeriesModule,
     EpisodiosModule,
